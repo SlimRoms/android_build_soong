3f8ddb9144309c7964a00311e53636442370429b
Prema Chand Alugu <premaca@gmail.com>
2018-08-14 13:48:23 +0530

    soong: Dump variables to our format liking
    * Add SLIM_VERSION to dumpvars
    * Add GCC version
    
    Change-Id: I2e54e285ced1038ceea27741537d3bed06617a7d

--- ui/build/dumpvars.go
+++ ui/build/dumpvars.go
@@ -125,6 +125,7 @@ func dumpMakeVars(ctx Context, config Config, goals, vars []string, write_soong_
 var BannerVars = []string{
 	"PLATFORM_VERSION_CODENAME",
 	"PLATFORM_VERSION",
+	"SLIM_VERSION",
 	"TARGET_PRODUCT",
 	"TARGET_BUILD_VARIANT",
 	"TARGET_BUILD_TYPE",
@@ -149,18 +150,49 @@ var BannerVars = []string{
 	"TARGET_BUILD_PDK",
 	"PDK_FUSION_PLATFORM_ZIP",
 	"PRODUCT_SOONG_NAMESPACES",
+	"TARGET_GCC_VERSION",
 }
 
 func Banner(make_vars map[string]string) string {
 	b := &bytes.Buffer{}
 
-	fmt.Fprintln(b, "============================================")
-	for _, name := range BannerVars {
-		if make_vars[name] != "" {
-			fmt.Fprintf(b, "%s=%s\n", name, make_vars[name])
-		}
-	}
-	fmt.Fprint(b, "============================================")
+	fmt.Fprintln(b, "===============================================================================")
+	fmt.Fprintln(b, "                              _______________________________________________  ")
+	fmt.Fprintln(b, "                             /                                  slimroms.org | ")
+	fmt.Fprintln(b, "                            /   _____________________________________________| ")
+	fmt.Fprintln(b, "                           /   /                                               ")
+	fmt.Fprintln(b, "                          /   /  _  _       ______                             ")
+	fmt.Fprintln(b, "                         /   /  | |(_)     (_____ \                            ")
+	fmt.Fprintln(b, "                        /   /   | | _ _____ _____) )___  _____  ___            ")
+	fmt.Fprintln(b, "  _____________________/   /    | || |     |  __  // _ \|     |/___)           ")
+	fmt.Fprintln(b, " |                        /     | || | | | | |  \ \ |_| | | | |___ |           ")
+	fmt.Fprintln(b, " |_______________________/       \_)_|_|_|_|_|   |_\___/|_|_|_(___/            ")
+	fmt.Fprintln(b, "===============================================================================")
+	fmt.Fprintf(b, "%s=%s\n", "HOST_ARCH", make_vars["HOST_ARCH"])
+	fmt.Fprintf(b, "%s=%s\n", "HOST_2ND_ARCH", make_vars["HOST_2ND_ARCH"])
+	fmt.Fprintf(b, "%s=%s\n", "HOST_OS", make_vars["HOST_OS"])
+	fmt.Fprintf(b, "%s=%s\n", "HOST_OS_EXTRA", make_vars["HOST_OS_EXTRA"])
+	fmt.Fprintf(b, "%s=%s\n", "HOST_CROSS_OS", make_vars["HOST_CROSS_OS"])
+	fmt.Fprintf(b, "%s=%s\n", "HOST_CROSS_ARCH", make_vars["HOST_CROSS_ARCH"])
+	fmt.Fprintf(b, "%s=%s\n", "HOST_CROSS_2ND_ARCH", make_vars["HOST_CROSS_2ND_ARCH"])
+	fmt.Fprintln(b, "=============================================================")
+	fmt.Fprintf(b, "%s=%s\n", "SLIM_VERSION", make_vars["SLIM_VERSION"])
+	fmt.Fprintf(b, "%s=%s\n", "BUILD_ID", make_vars["BUILD_ID"])
+	fmt.Fprintf(b, "%s=%s\n", "OUT_DIR", make_vars["OUT_DIR"])
+	fmt.Fprintln(b, "=============================================================")
+	fmt.Fprintf(b, "%s=%s\n", "PLATFORM_VERSION_CODENAME", make_vars["PLATFORM_VERSION_CODENAME"])
+	fmt.Fprintf(b, "%s=%s\n", "PLATFORM_VERSION", make_vars["PLATFORM_VERSION"])
+	fmt.Fprintf(b, "%s=%s\n", "TARGET_PRODUCT", make_vars["TARGET_PRODUCT"])
+	fmt.Fprintf(b, "%s=%s\n", "TARGET_BUILD_VARIANT", make_vars["TARGET_BUILD_VARIANT"])
+	fmt.Fprintf(b, "%s=%s\n", "TARGET_BUILD_TYPE", make_vars["TARGET_BUILD_TYPE"])
+	fmt.Fprintf(b, "%s=%s\n", "TARGET_GCC_VERSION", make_vars["TARGET_GCC_VERSION"])
+	fmt.Fprintf(b, "%s=%s\n", "TARGET_ARCH", make_vars["TARGET_ARCH"])
+	fmt.Fprintf(b, "%s=%s\n", "TARGET_ARCH_VARIANT", make_vars["TARGET_ARCH_VARIANT"])
+	fmt.Fprintf(b, "%s=%s\n", "TARGET_CPU_VARIANT", make_vars["TARGET_CPU_VARIANT"])
+	fmt.Fprintf(b, "%s=%s\n", "TARGET_2ND_ARCH", make_vars["TARGET_2ND_ARCH"])
+	fmt.Fprintf(b, "%s=%s\n", "TARGET_2ND_ARCH_VARIANT", make_vars["TARGET_2ND_ARCH_VARIANT"])
+	fmt.Fprintf(b, "%s=%s\n", "TARGET_2ND_CPU_VARIANT", make_vars["TARGET_2ND_CPU_VARIANT"])
+	fmt.Fprintln(b, "=============================================================")
 
 	return b.String()
 }
